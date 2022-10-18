3507e457ae81767ac3f1e9ba8c1207dbc8ddad1e
Ilya Maslikov <ilya.maslik@gmail.com>
2022-10-18 14:46:32 +0300

    Second commit

--- Assets/Skripts/CameraManager.cs
+++ Assets/Skripts/CameraManager.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    [Header("Где наш супермен")]
-    [SerializeField] private Transform superMan;
-    
-    [Header("Расстояние от него до камеры")]
-    private Vector3 offset;
+    [Header("Игрок")]
+    [SerializeField] private Transform SuperMan;
+
+    [Header("Расстояние от камеры до игрока")] 
+    private Vector3 _offset;
 
     private void Start()
     {
-        offset = transform.position - superMan.position;
+        _offset = transform.position - SuperMan.position;
     }
 
     private void FixedUpdate()
     {
-        transform.position = superMan.position + offset;
+        transform.position = SuperMan.position + _offset;
     }
 }
